32de190be6339861788f94ac1f6eab5d8c6b2440
gwangWook218 <akw2080@naver.com>
2024-08-08 16:59:19 +0900

    0808am

---
+++ ex0808/Proj0808/src/ch06/sec07/exam01/CarExample.java
@@ -0,0 +1,11 @@
+package ch06.sec07.exam01;
+
+public class CarExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car myCar = new Car("EV9", "흰색", 300);
+//		Car myCar = new Car();  //기본 생성자 호출 못함
+	}
+	
+}
