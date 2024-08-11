4ff2cdb5b541d38f3288fcbc4d9ca5f88c915acc
gwangWook218 <akw2080@naver.com>
2024-08-11 15:58:21 +0900

    0809am

--- ex0809/Proj0809/src/ch06/sec13/exam03/package2/C.java
+++ ex0809/Proj0809/src/ch06/sec13/exam03/package2/C.java
@@ -1,11 +1,13 @@
-package ch06.sec13.exam03.package1;
+package ch06.sec13.exam03.package2;
 
-public class B {
-	public void method() {
+import ch06.sec13.exam03.package1.A;
+
+public class C {
+	public C() {
 //		객체 생성
 		A a = new A();
 		
-//		필드값 생성
+//		필드값 변경
 		a.field1 = 1;
 		a.field2 = 1;
 		a.field3 = 1;
