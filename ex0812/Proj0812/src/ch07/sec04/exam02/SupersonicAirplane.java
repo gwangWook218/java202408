87edffc83465c669f8231eb8b73f6ea4c9d1246e
gwangWook218 <akw2080@naver.com>
2024-08-12 17:09:35 +0900

    0812am

---
+++ ex0812/Proj0812/src/ch07/sec04/exam02/SupersonicAirplane.java
@@ -0,0 +1,24 @@
+package ch07.sec04.exam02;
+
+public class SupersonicAirplane extends Airplane {
+//	상수 선언
+	public static final int NORMAL = 1;
+	public static final int SUPERSONIC = 2;
+//	상태 필드 선언
+	public int flyNode = NORMAL;
+	
+	
+	
+//	메소드 재정의
+	@Override
+	public void fly() {
+		if(flyNode == SUPERSONIC) {
+			System.out.println("초음속 비행합니다.");
+		} else {
+//			Airplane 객체의 fly() 메소드 호출
+			super.fly();
+		}
+	}
+	
+	
+}
